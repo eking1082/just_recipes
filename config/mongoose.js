9b46adf31c7cbfa2ab16477a86bf77988df1da81
YOUR NAME <YOUR@EMAIL.com>
2019-10-07 18:40:07 -0500

    correct mongo db uri

--- config/mongoose.js
+++ config/mongoose.js
@@ -5,16 +5,11 @@ mongoose.Promise = bluebird;
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useNewUrlParser', true);
-mongoose.connect('mongodb://localhost:27017/just_recipes');
+mongoose.connect(process.env.MONGODB_URI);
+
 
 mongoose.connection.on('error', (err) => {
   console.error(err);
   console.log('%s MongoDB connection error. Please make sure MongoDB is running.');
   process.exit();
 });
-
-// exports.savePage = (url) => rp(url).then((html) => {
-//   const page = new Page({ url, html });
-//   console.log(page.url);
-//   return page.save();
-// });
